69891ac0ec27a7df0ee7ddec1433bbf9ff6af2f9
fazyarsa <308948095+fazya79-create@users.noreply.github.com>
2026-09-06 00:38:31 +0000

    strip: trim template constraint verifier

--- utilities/templates-impl/src/main/java/com/itsaky/androidide/templates/impl/ConstraintVerifier.kt
+++ utilities/templates-impl/src/main/java/com/itsaky/androidide/templates/impl/ConstraintVerifier.kt
@@ -19,19 +19,13 @@ package com.itsaky.androidide.templates.impl
 
 import com.itsaky.androidide.app.BaseApplication
 import com.itsaky.androidide.templates.ParameterConstraint
-import com.itsaky.androidide.templates.ParameterConstraint.CLASS
-import com.itsaky.androidide.templates.ParameterConstraint.CLASS_NAME
 import com.itsaky.androidide.templates.ParameterConstraint.DIRECTORY
 import com.itsaky.androidide.templates.ParameterConstraint.EXISTS
 import com.itsaky.androidide.templates.ParameterConstraint.FILE
-import com.itsaky.androidide.templates.ParameterConstraint.LAYOUT
 import com.itsaky.androidide.templates.ParameterConstraint.MODULE_NAME
 import com.itsaky.androidide.templates.ParameterConstraint.NONEMPTY
-import com.itsaky.androidide.templates.ParameterConstraint.PACKAGE
 import com.itsaky.androidide.templates.base.util.isValidModuleName
 import com.itsaky.androidide.templates.impl.R.string
-import com.itsaky.androidide.utils.AndroidUtils
-import jdkx.lang.model.SourceVersion
 import java.io.File
 
 /**
@@ -61,11 +55,7 @@ object ConstraintVerifier {
     for (constraint in constraints) {
       err = when (constraint) {
         NONEMPTY -> checkNotEmpty(input)
-        PACKAGE -> validatePackageName(input)
-        CLASS -> validateClassName(input)
-        CLASS_NAME -> validateSimpleName(input)
         MODULE_NAME -> validateModuleName(input)
-        LAYOUT -> validateLayoutName(input)
         EXISTS -> checKFileExists(input)
         FILE -> checkIsFile(input)
         DIRECTORY -> checkIsDirectory(input)
@@ -78,32 +68,6 @@ object ConstraintVerifier {
       }
     }
     return null
-  }
-
-  private fun validatePackageName(input: String): String? {
-    AndroidUtils.validatePackageName(input)?.let { return it }
-
-    if (SourceVersion.isName(input)) {
-      return null
-    }
-
-    return BaseApplication.getBaseInstance()
-      .getString(R.string.msg_package_is_not_valid)
-  }
-
-  private fun validateLayoutName(input: String): String? {
-    if (input.isBlank()) {
-      return BaseApplication.getBaseInstance()
-        .getString(R.string.msg_value_empty)
-    }
-
-    // Allow only lowecase letters with digits and underscores
-    if (input.matches(Regex("[a-z][a-z0-9_]+"))) {
-      return null
-    }
-
-    return BaseApplication.getBaseInstance()
-      .getString(string.msg_invalid_layout_name)
   }
 
   private fun checkIsDirectory(input: String): String? {
@@ -155,36 +119,5 @@ object ConstraintVerifier {
 
     return BaseApplication.getBaseInstance()
       .getString(string.msg_invalid_module_name)
-  }
-
-  private fun validateClassName(name: String): String? {
-    if (!name.contains('.')) {
-      return validateSimpleName(name)
-    }
-
-    val pck = name.substring(0, name.lastIndexOf('.'))
-    var err: String? = null
-    if (pck.contains('.')) {
-      err = validatePackageName(pck)
-    } else {
-      if (!SourceVersion.isIdentifier(pck) || SourceVersion.isKeyword(pck)) {
-        err = BaseApplication.getBaseInstance()
-          .getString(string.msg_package_is_not_valid)
-      }
-    }
-
-    if (err != null) {
-      return err
-    }
-
-    return validateSimpleName(name.substring(name.lastIndexOf('.') + 1))
-  }
-
-  private fun validateSimpleName(name: String): String? {
-    if (SourceVersion.isKeyword(name) || !SourceVersion.isIdentifier(name)) {
-      return BaseApplication.getBaseInstance()
-        .getString(string.msg_classname_with_keywords)
-    }
-    return null
   }
 }
